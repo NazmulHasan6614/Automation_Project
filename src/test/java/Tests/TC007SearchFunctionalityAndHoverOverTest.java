bef84853987e5fa142e46bb1f08aa2193e652fb3
unknown <NazmuL>
2024-05-21 00:14:54 +0600

    All done

--- src/test/java/Tests/TC007SearchFunctionalityAndHoverOverTest.java
+++ src/test/java/Tests/TC007SearchFunctionalityAndHoverOverTest.java
@@ -1,11 +1,14 @@
 package Tests;
 
 import Pages.AutomationPracticeSearchField;
+import Pages.MouseHoverOver;
 import org.testng.annotations.Test;
 
-public class TC007TestSearchFunctionalityTest extends Base{
+public class TC007SearchFunctionalityAndHoverOverTest extends Base{
     AutomationPracticeSearchField
     searchObject=new AutomationPracticeSearchField();
+    MouseHoverOver
+            hoverOverObject=new MouseHoverOver();
     @Test
     public void validSearch()
     {
@@ -18,5 +21,10 @@ public class TC007TestSearchFunctionalityTest extends Base{
     @Test public void BlankSearch()
     {
         searchObject.testSearchWithBlankData(driver);
+    }
+    @Test
+    public void hoverOver()
+    {
+        hoverOverObject.hoverOver(driver);
     }
 }
